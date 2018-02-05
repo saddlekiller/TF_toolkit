30319bb2d49f757946029b362827989cbd38b9f3
Bi <cheng.bi@nuance.com>
2018-02-05 18:45:34 +0800

    Update seq2seq

--- experiment/seq2seq_basic.py
+++ experiment/seq2seq_basic.py
@@ -11,29 +11,91 @@ from config_mapping import *
 from tools import *
 from layers import *
 from tensorflow.contrib.seq2seq import *
+from params import *
+
 
 corpus_dir = '../../Basic_Tensorflow/corpus/'
 label_map = pickle.load(open(corpus_dir + 'poilabel_map.npz', 'rb'))
 dictionary = pickle.load(open(corpus_dir + 'poiwords.dict', 'rb'))
 
+class idProvider():
+
+    def __init__(self, filename, batch_size):
+        self.filename = filename
+        self.batch_size = batch_size
+        self._currentPosition = 0
+        self.loadCorpus()
+        # for key in self.batched_inputs.keys():
+        #     print(key, len(self.batched_inputs[key]))
+
+    def reset(self):
+        self._currentOrder = self.keys.copy()
+        self.new_epoch()
+
+    def new_epoch(self):
+        np.random.shuffle(self._currentOrder)
+        self._currentPosition = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._currentPosition >= self.n_batches:
+            self.new_epoch()
+            raise StopIteration
+        batch_input = self.batched_inputs[self._currentOrder[self._currentPosition]]
+        batch_target = self.batched_targets[self._currentOrder[self._currentPosition]]
+        length = int(self._currentOrder[self._currentPosition].split('_')[0])
+        # print(self._currentOrder[self._currentPosition])
+        self._currentPosition += 1
+        return length, batch_input, batch_target
+
+    def next(self):
+        return self.__next__()
+
+    def loadCorpus(self):
+        lines = open(self.filename).readlines()
+        varLen_inputs = dict()
+        varLen_targets = dict()
+        for line in lines:
+            splited = line.split()
+            ids = [int(i) for i in splited[1:]]
+            targets = int(splited[0])
+            key = len(ids)
+            if key not in varLen_inputs.keys():
+                varLen_inputs[key] = []
+                varLen_targets[key] = []
+            varLen_inputs[key].append(ids)
+            varLen_targets[key].append(targets)
+        self.batched_inputs = dict()
+        self.batched_targets = dict()
+        for key in varLen_targets.keys():
+            if len(varLen_targets[key])%self.batch_size == 0:
+                count = int(len(varLen_targets[key])/self.batch_size)
+            else:
+                count = int(len(varLen_targets[key])/self.batch_size) + 1
+            for i in range(count):
+                new_key = str(key) + '_' + str(i)
+                if i == count - 1:
+                    self.batched_inputs[new_key] = varLen_inputs[key][i*self.batch_size:]
+                    self.batched_targets[new_key] = varLen_targets[key][i*self.batch_size:]
+                else:
+                    self.batched_inputs[new_key] = varLen_inputs[key][i*self.batch_size:(i+1)*self.batch_size]
+                    self.batched_targets[new_key] = varLen_targets[key][i*self.batch_size:(i+1)*self.batch_size]
+
+        self.keys = list(self.batched_targets.keys())
+        self.n_batches = len(self.keys)
+        self.reset()
+
 max_word = 35
 voc_size = len(dictionary)
 embedding_size = 128
-train_data = varLenDataProvider(corpus_dir + 'anouymous_corpus_full_train.txt', batch_size = 50, label_map = label_map, dictionary = dictionary, max_word = max_word)
-
-inputs_placeholder = tf.placeholder(tf.float32, [None, None, voc_size])
-
-
-shape = tf.shape(inputs_placeholder)
-reshape_layer1 = reshape_layer('reshape_1', [shape[0] * shape[1], voc_size])
-affine_layer1 = affine_layer('affine_1', [voc_size, embedding_size], tf.nn.relu)
-reshape_layer2 = reshape_layer('reshape_2', [shape[0], shape[1], embedding_size])
-lstm_layer1 = lstm_layer('lstm_1', embedding_size)
+provider = idProvider(corpus_dir + 'anouymous_corpus_full_train.txt', 50)
+# for length, inputs, targets in provider:
+#     print(length, len(inputs))
+    # pass
 
 
-reshape_layer1_o = reshape_layer1.outputs(inputs_placeholder)
-affine_layer1_o = affine_layer1.outputs(reshape_layer1_o)
-reshape_layer2_o = reshape_layer2.outputs(affine_layer1_o)
-lstm_layer1_o = lstm_layer1.outputs(reshape_layer2_o)
-
-helper = TrainingHelper(target_embeddeds, self.decoder_seq_length)
+inputs_placeholder = tf.placeholder(tf.float32, [None, None], name = 'inputs_ids')
+seq_length = tf.placeholder(tf.int32, 'sequence_length')
+embedding = truncated_normal('embedding', [])
