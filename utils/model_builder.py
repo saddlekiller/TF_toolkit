3d2aa95c462dcfe031d7d783ecafc445fd43f0d3
Cheng.Bi <Cheng.Bi@nuance.com>
2018-02-06 15:21:01 +0800

    integrate exist seq2seq model

--- utils/model_builder.py
+++ utils/model_builder.py
@@ -7,6 +7,8 @@ from data_provider import *
 from logging_io import *
 import shutil
 import os
+from tensorflow.contrib.seq2seq import *
+from tensorflow.python.layers.core import Dense
 
 def basic_builder(data_conf_dir, model_conf_dir, data_provider, round_val=5, saveLOG=True, needSummary = False, isAutoEncoder = False):
 
@@ -147,6 +149,7 @@ def basic_builder(data_conf_dir, model_conf_dir, data_provider, round_val=5, sav
         pickle.dump(bestModels_id, open('model_ids.npz', 'wb'))
     return log
 
+'''
 def autoencoder_builder(data_conf_dir, model_conf_dir, data_provider, round_val=5, saveLOG=True):
 
     data_conf = json.load(open(data_conf_dir, 'r'))
@@ -229,3 +232,93 @@ def autoencoder_builder(data_conf_dir, model_conf_dir, data_provider, round_val=
         if saveLOG == True:
             logging_io.LOG_COLLECTOR('LOGS')
     return log
+'''
+
+class Model(object):
+
+    def __init__(self):
+        pass
+
+
+class Seq2SeqModel(object):
+
+    def __init__(self, rnn_size, layer_size, encoder_vocab_size,
+        decoder_vocab_size, embedding_dim, grad_clip, is_inference=False):
+        # define inputs
+        self.input_x = tf.placeholder(tf.int32, shape=[None, None], name='input_ids')
+
+        # define embedding layer
+        with tf.variable_scope('embedding'):
+            encoder_embedding = tf.Variable(tf.truncated_normal(shape=[encoder_vocab_size, embedding_dim], stddev=0.1),
+                name='encoder_embedding')
+            decoder_embedding = tf.Variable(tf.truncated_normal(shape=[decoder_vocab_size, embedding_dim], stddev=0.1),
+                name='decoder_embedding')
+
+        # define encoder
+        with tf.variable_scope('encoder'):
+            encoder = self._get_simple_lstm(rnn_size, layer_size)
+
+        with tf.device('/cpu:0'):
+            input_x_embedded = tf.nn.embedding_lookup(encoder_embedding, self.input_x)
+
+        encoder_outputs, encoder_state = tf.nn.dynamic_rnn(encoder, input_x_embedded, dtype=tf.float32)
+
+        # define helper for decoder
+        if is_inference:
+            self.start_tokens = tf.placeholder(tf.int32, shape=[None], name='start_tokens')
+            self.end_token = tf.placeholder(tf.int32, name='end_token')
+            helper = GreedyEmbeddingHelper(decoder_embedding, self.start_tokens, self.end_token)
+        else:
+            self.target_ids = tf.placeholder(tf.int32, shape=[None, None], name='target_ids')
+            self.decoder_seq_length = tf.placeholder(tf.int32, shape=[None], name='batch_seq_length')
+            with tf.device('/cpu:0'):
+                target_embeddeds = tf.nn.embedding_lookup(decoder_embedding, self.target_ids)
+            helper = TrainingHelper(target_embeddeds, self.decoder_seq_length)
+
+        with tf.variable_scope('decoder'):
+            fc_layer = Dense(decoder_vocab_size)
+            decoder_cell = self._get_simple_lstm(rnn_size, layer_size)
+            decoder = BasicDecoder(decoder_cell, helper, encoder_state, fc_layer)
+
+        logits, final_state, final_sequence_lengths = dynamic_decode(decoder)
+
+        if not is_inference:
+            targets = tf.reshape(self.target_ids, [-1])
+            logits_flat = tf.reshape(logits.rnn_output, [-1, decoder_vocab_size])
+            print('shape logits_flat:{}'.format(logits_flat.shape))
+            print('shape logits:{}'.format(logits.rnn_output.shape))
+
+            self.cost = tf.losses.sparse_softmax_cross_entropy(targets, logits_flat)
+
+            # define train op
+            tvars = tf.trainable_variables()
+            grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), grad_clip)
+
+            optimizer = tf.train.AdamOptimizer(1e-3)
+            self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+        else:
+            self.prob = tf.nn.softmax(logits)
+
+    def _get_simple_lstm(self, rnn_size, layer_size):
+        lstm_layers = [tf.contrib.rnn.LSTMCell(rnn_size) for _ in np.arange(layer_size)]
+        return tf.contrib.rnn.MultiRNNCell(lstm_layers)
+
+if __name__ == '__main__':
+    corpus_dir = '../../Basic_Tensorflow/corpus/'
+    label_map = pickle.load(open(corpus_dir + 'label_map.npz', 'rb'))
+    dictionary = pickle.load(open(corpus_dir + 'words.dict', 'rb'))
+    max_word = 35
+    voc_size = len(dictionary)
+    embedding_size = 128
+    provider = idProvider(corpus_dir + 'anouymous_corpus_full_train.txt', 50) #anonymous_raw_poi_train.txt
+
+
+    graph = tf.Graph()
+    with graph.as_default():
+        model = Seq2SeqModel(64, 1, voc_size, voc_size, 100, 10, False)
+        sess = tf.Session()
+        sess.run(tf.global_variables_initializer())
+        for length, batch_input, batch_targets in provider:
+            feed_dict = {model.input_x:batch_input, model.target_ids:batch_input, model.decoder_seq_length:[length]*len(batch_input)}
+            _, loss = sess.run([model.train_op, model.cost], feed_dict = feed_dict)
+            tf.logging.info('LOSS is : ' + str(loss))
