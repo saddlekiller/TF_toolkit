d2a0d60277e92e07f3da729dffeaf9321fe52606
Cheng <alanbi1994@gmail.com>
2018-02-26 23:18:11 +0800

    Update rebuild module

--- models/autoencoder_analysis/basic_affine.py
+++ models/autoencoder_analysis/basic_affine.py
@@ -18,6 +18,7 @@ hidden_dim3 = 10
 graph = tf.Graph()
 with graph.as_default():
     inputs_placeholder = tf.placeholder(tf.float32, [None, input_dim])
+    code_placeholder = tf.placeholder(tf.float32, [None, hidden_dim3])
     with tf.variable_scope('encoder_1'):
         e_w1 = tf.get_variable('e_w1', shape = [input_dim, hidden_dim1], initializer = tf.truncated_normal_initializer)
         e_b1 = tf.get_variable('e_b1', shape = [hidden_dim1], initializer = tf.truncated_normal_initializer)
@@ -37,38 +38,54 @@ with graph.as_default():
         d_w1 = tf.get_variable('d_w1', shape = [hidden_dim3, hidden_dim2], initializer = tf.truncated_normal_initializer)
         d_b1 = tf.get_variable('d_b1', shape = [hidden_dim2], initializer = tf.truncated_normal_initializer)
         d_o1 = tf.nn.sigmoid(tf.add(tf.matmul(e_o3, d_w1), d_b1))
+        rebuild_o1 = tf.nn.sigmoid(tf.add(tf.matmul(code_placeholder, d_w1), d_b1))
 
     with tf.variable_scope('decoder_2'):
         d_w2 = tf.get_variable('d_w2', shape = [hidden_dim2, hidden_dim1], initializer = tf.truncated_normal_initializer)
         d_b2 = tf.get_variable('d_b2', shape = [hidden_dim1], initializer = tf.truncated_normal_initializer)
         d_o2 = tf.nn.sigmoid(tf.add(tf.matmul(d_o1, d_w2), d_b2))
+        rebuild_o2 = tf.nn.sigmoid(tf.add(tf.matmul(rebuild_o1, d_w2), d_b2))
 
     with tf.variable_scope('decoder_3'):
         d_w3 = tf.get_variable('d_w3', shape = [hidden_dim1, input_dim], initializer = tf.truncated_normal_initializer)
         d_b3 = tf.get_variable('d_b3', shape = [input_dim], initializer = tf.truncated_normal_initializer)
         d_o3 = tf.nn.sigmoid(tf.add(tf.matmul(d_o2, d_w3), d_b3))
+        rebuild_o3 = tf.nn.sigmoid(tf.add(tf.matmul(rebuild_o2, d_w3), d_b3))
+        rebuild = tf.reshape(rebuild_o3, [-1, 28, 28])
 
     loss = tf.reduce_mean((d_o3 - inputs_placeholder)**2)
     optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
-    for i in range(20):
+    for i in range(10):
         errs = []
         for batch_inputs, batch_targets in provider:
-            feed_dict = {inputs_placeholder: batch_inputs}
+            feed_dict = {inputs_placeholder: batch_inputs, code_placeholder:np.zeros((1, hidden_dim3))}
             _, err = sess.run([optimizer, loss], feed_dict = feed_dict)
             errs.append(err)
         print('#Epoch %d, Loss: %f'%(i, np.mean(errs)))
     hidden_units = {}
     for batch_inputs, batch_targets in provider:
-        feed_dict = {inputs_placeholder: batch_inputs}
+        feed_dict = {inputs_placeholder: batch_inputs, code_placeholder:np.zeros((1, hidden_dim3))}
         features = sess.run(e_o3, feed_dict = feed_dict)
         for target, feature in zip(batch_targets, features):
             key = np.argmax(target)
             if key not in hidden_units.keys():
                 hidden_units[key] = []
             hidden_units[key].append(feature)
+    means = [None]*hidden_dim3
     for key in hidden_units.keys():
         hidden_units[key] = np.array(hidden_units[key])
-    pickle.dump(hidden_units, open('features.npz', 'wb'))
+        means[key] = np.mean(hidden_units[key], 0)
+    feed_dict = {inputs_placeholder: batch_inputs, code_placeholder:means}
+    image = sess.run(rebuild, feed_dict = feed_dict)
+    print(image.shape)
+    import matplotlib.pyplot as plt
+    f = plt.figure()
+    for i in range(10):
+        ax = f.add_subplot(5, 2, i+1)
+        ax.imshow(image[i])
+        plt.title(i)
+    plt.show()
+    # pickle.dump(hidden_units, open('features.npz', 'wb'))
