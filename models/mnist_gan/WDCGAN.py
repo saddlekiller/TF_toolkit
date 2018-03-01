69870bf8da4a541cad3b14e76e944492f8a55f5c
Bi <cheng.bi@nuance.com>
2018-03-01 17:55:15 +0800

    replace with tensorflow contrib

--- models/mnist_gan/WDCGAN.py
+++ models/mnist_gan/WDCGAN.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append('../../utils')
 from data_provider import *
 from tools import *
-
+from tensorflow.contrib.layers import *
 
 
 batch_size = 100
@@ -29,31 +29,55 @@ with graph.as_default():
     #
         with tf.variable_scope('Generator'):
 
-            Generator_w1 = tf.get_variable('Generator_w1', initializer = tf.truncated_normal([output_dim, 128]))
-            Generator_b1 = tf.get_variable('Generator_b1', initializer = tf.truncated_normal([128]))
-            Generator_k2 = tf.get_variable('Generator_w2', initializer = tf.truncated_normal([7, 7, 64, 128]))
-            Generator_b2 = tf.get_variable('Generator_b2', initializer = tf.truncated_normal([64]))
-            Generator_k3 = tf.get_variable('Generator_w3', initializer = tf.truncated_normal([2, 2, 16, 64]))
-            Generator_b3 = tf.get_variable('Generator_b3', initializer = tf.truncated_normal([16]))
-            Generator_k4 = tf.get_variable('Generator_w4', initializer = tf.truncated_normal([2, 2, 1, 16]))
-            Generator_b4 = tf.get_variable('Generator_b4', initializer = tf.truncated_normal([1]))
-
-            Generator_affine_1  = tf.nn.relu(tf.add(tf.matmul(inputs      , Generator_w1), Generator_b1))
+            Generator_affine_1  = fully_connected(inputs, 128,
+                                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                                # weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
+                                activation_fn=tf.nn.relu)
             Generator_reshape_1 = tf.reshape(Generator_affine_1, [batch_size, 1, 1, 128])
-            Generator_deconv_1  = tf.nn.relu(tf.add(tf.nn.conv2d_transpose(Generator_reshape_1, Generator_k2, [batch_size, 7, 7, 64], [1, 7, 7, 1], 'SAME'), Generator_b2))
-            Generator_deconv_2  = tf.nn.relu(tf.add(tf.nn.conv2d_transpose(Generator_deconv_1, Generator_k3, [batch_size, 14, 14, 16], [1, 2, 2, 1], 'SAME'), Generator_b3))
-            Generator_deconv_3  = tf.nn.sigmoid(tf.add(tf.nn.conv2d_transpose(Generator_deconv_2, Generator_k4, [batch_size, 28, 28, 1], [1, 2, 2, 1], 'SAME'), Generator_b4))
+            Generator_deconv_1  = convolution2d_transpose(Generator_reshape_1, 64, [7, 7], [7, 7],
+                                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                                # weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
+                                activation_fn=tf.nn.relu)
 
-            print('*'*49)
-            print('*'*1 + ' '*19 + 'Generator' + ' '*19 + '*'*1)
-            print('*'*49)
-            print(Generator_affine_1)
-            print(Generator_reshape_1)
-            print(Generator_deconv_1)
-            print(Generator_deconv_2)
-            print(Generator_deconv_3)
-            print('*'*49)
+            Generator_deconv_2  = convolution2d_transpose(Generator_deconv_1, 16, [2, 2], [2, 2],
+                                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                                # weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
+                                activation_fn=tf.nn.relu)
 
+            Generator_deconv_3  = convolution2d_transpose(Generator_deconv_2, 1, [2, 2], [2, 2],
+                                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                                # weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
+                                activation_fn=tf.nn.sigmoid)
+
+            # print(Generator_affine_1)
+            # print(Generator_deconv_1)
+            # print(Generator_deconv_2)
+            # print(Generator_deconv_3)
+            # Generator_w1 = tf.get_variable('Generator_w1', initializer = tf.truncated_normal([output_dim, 128]))
+            # Generator_b1 = tf.get_variable('Generator_b1', initializer = tf.truncated_normal([128]))
+            # Generator_k2 = tf.get_variable('Generator_w2', initializer = tf.truncated_normal([7, 7, 64, 128]))
+            # Generator_b2 = tf.get_variable('Generator_b2', initializer = tf.truncated_normal([64]))
+            # Generator_k3 = tf.get_variable('Generator_w3', initializer = tf.truncated_normal([2, 2, 16, 64]))
+            # Generator_b3 = tf.get_variable('Generator_b3', initializer = tf.truncated_normal([16]))
+            # Generator_k4 = tf.get_variable('Generator_w4', initializer = tf.truncated_normal([2, 2, 1, 16]))
+            # Generator_b4 = tf.get_variable('Generator_b4', initializer = tf.truncated_normal([1]))
+            #
+            # Generator_affine_1  = tf.nn.relu(tf.add(tf.matmul(inputs      , Generator_w1), Generator_b1))
+            # Generator_reshape_1 = tf.reshape(Generator_affine_1, [batch_size, 1, 1, 128])
+            # Generator_deconv_1  = tf.nn.relu(tf.add(tf.nn.conv2d_transpose(Generator_reshape_1, Generator_k2, [batch_size, 7, 7, 64], [1, 7, 7, 1], 'SAME'), Generator_b2))
+            # Generator_deconv_2  = tf.nn.relu(tf.add(tf.nn.conv2d_transpose(Generator_deconv_1, Generator_k3, [batch_size, 14, 14, 16], [1, 2, 2, 1], 'SAME'), Generator_b3))
+            # Generator_deconv_3  = tf.nn.sigmoid(tf.add(tf.nn.conv2d_transpose(Generator_deconv_2, Generator_k4, [batch_size, 28, 28, 1], [1, 2, 2, 1], 'SAME'), Generator_b4))
+
+        #     print('*'*49)
+        #     print('*'*1 + ' '*19 + 'Generator' + ' '*19 + '*'*1)
+        #     print('*'*49)
+        #     print(Generator_affine_1)
+        #     print(Generator_reshape_1)
+        #     print(Generator_deconv_1)
+        #     print(Generator_deconv_2)
+        #     print(Generator_deconv_3)
+        #     print('*'*49)
+        #
         return Generator_deconv_3
 
     def Discriminator(inputs, reuse = False):
@@ -112,42 +136,46 @@ with graph.as_default():
 
     Generator_variables     = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Generator")
     Discriminator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator")
+    print(Generator_variables)
 
-    Generator_optimizer     = tf.train.RMSPropOptimizer(0.00005).minimize(Generator_loss,     var_list = Generator_variables)
-    Discriminator_optimizer = tf.train.RMSPropOptimizer(0.00005) .minimize(Discriminator_loss, var_list = Discriminator_variables)
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        Generator_optimizer     = tf.train.RMSPropOptimizer(0.00005).minimize(Generator_loss,     var_list = Generator_variables)
+        Discriminator_optimizer = tf.train.RMSPropOptimizer(0.00005) .minimize(Discriminator_loss, var_list = Discriminator_variables)
+
+
     Clip = [v.assign(tf.clip_by_value(v, -0.01, 0.01)) for v in Discriminator_variables]
-    # a = [var for var in tf.global_variables() if 'Discriminator' in var.name]
-    # Clip = [v for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator")]
-    # print('-'*50)
-    # for v in Clip:
-    #     print(v)
-    # print('-'*50)
-    # for ai in a:
-    #     print(ai)
-    # print('-'*50)
+    a = [var for var in tf.global_variables() if 'Discriminator' in var.name]
+    Clip = [v for v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="Discriminator")]
+    print('-'*50)
+    for v in Clip:
+        print(v)
+    print('-'*50)
+    for ai in a:
+        print(ai)
+    print('-'*50)
 
-    # sess = tf.Session()
-    # sess.run(tf.global_variables_initializer())
-    # # merged_all = tf.summary.merge_all()
-    # writer = tf.summary.FileWriter('tensorboard', sess.graph)
-    #
-    #
-    # for i in range(500):
-    #     d_losses = []
-    #     g_losses = []
-    #     for batch_inputs, batch_targets in provider:
-    #         for j in range(5):
-    #             noise = np.random.uniform(-1, 1, [batch_size, output_dim]).astype(np.float32)
-    #             feed_dict = {data_placeholder: batch_inputs.reshape(-1, 28, 28, 1), prior_placeholder: noise}
-    #             sess.run(Clip)
-    #             _, d_loss          = sess.run([Discriminator_optimizer, Discriminator_loss]       , feed_dict = feed_dict)
-    #         noise = np.random.uniform(-1, 1, [batch_size, output_dim]).astype(np.float32)
-    #         feed_dict = {data_placeholder: batch_inputs.reshape(-1, 28, 28, 1), prior_placeholder: noise}
-    #         _, g_loss, g_image = sess.run([Generator_optimizer, Generator_loss, Generator_out], feed_dict = feed_dict)
-    #
-    #         d_losses.append(d_loss)
-    #         g_losses.append(g_loss)
-    #     print('EPOCH %d, D_LOSS: %f, G_LOSS: %f '%(i, np.mean(d_losses), np.mean(g_losses)))
-    #     g_image = g_image.reshape([-1, 28, 28]).transpose([1,2,0])
-    #     merge_image = build_image(g_image, 10)
-    #     plt.imsave(str(i)+'.png', merge_image)
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    # merged_all = tf.summary.merge_all()
+    writer = tf.summary.FileWriter('tensorboard', sess.graph)
+
+
+    for i in range(500):
+        d_losses = []
+        g_losses = []
+        for batch_inputs, batch_targets in provider:
+            for j in range(5):
+                noise = np.random.uniform(-1, 1, [batch_size, output_dim]).astype(np.float32)
+                feed_dict = {data_placeholder: batch_inputs.reshape(-1, 28, 28, 1), prior_placeholder: noise}
+                sess.run(Clip)
+                _, d_loss          = sess.run([Discriminator_optimizer, Discriminator_loss]       , feed_dict = feed_dict)
+            noise = np.random.uniform(-1, 1, [batch_size, output_dim]).astype(np.float32)
+            feed_dict = {data_placeholder: batch_inputs.reshape(-1, 28, 28, 1), prior_placeholder: noise}
+            _, g_loss, g_image = sess.run([Generator_optimizer, Generator_loss, Generator_out], feed_dict = feed_dict)
+
+            d_losses.append(d_loss)
+            g_losses.append(g_loss)
+        print('EPOCH %d, D_LOSS: %f, G_LOSS: %f '%(i, np.mean(d_losses), np.mean(g_losses)))
+        g_image = g_image.reshape([-1, 28, 28]).transpose([1,2,0])
+        merge_image = build_image(g_image, 10)
+        plt.imsave(str(i)+'.png', merge_image)
