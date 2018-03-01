90829988b7c177f65efc7775d36e6410629700a3
Cheng <alanbi1994@gmail.com>
2018-03-01 22:57:38 +0800

    Update image builder

--- utils/tools.py
+++ utils/tools.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from config_mapping import *
 import pickle
+import matplotlib.pyplot as plt
 
 
 def acc_sum(results, targets):
@@ -70,11 +71,34 @@ def build_image(inputs, n_rows, bounder_size = 4):
     # plt.imshow(image)
     # plt.show()
 
-# if __name__ == '__main__':
-#     data = []
-#     for i in range(16):
-#         temp = np.random.random((28, 28))
-#         data.append(temp)
-#     data = np.array(data).reshape([28, 28, 16])
-#     print(data.shape)
-#     build_image(data, 4)
+
+def build_image_(inputs, n_rows, bounder_size = 4):
+    n, height, width, channel = inputs.shape
+    n = int(n)
+    height = int(height)
+    width = int(width)
+    channel = int(channel)
+    # print(int(inputs.shape[0]))
+    n_cols = int(n / n_rows)
+    if int(n / n_rows) != (n / n_rows):
+        raise TypeError
+    x_size = (n_rows + 1) * bounder_size + n_rows * height
+    y_size = (n_cols + 1) * bounder_size + n_cols * width
+    image = np.zeros((x_size, y_size, channel))
+    for i in range(n_rows):
+        for j in range(n_cols):
+            for k in range(3):
+                image[(i+1) * bounder_size + i * width: (i+1) * bounder_size + (i + 1) * width, (j+1) * bounder_size + j * height: (j+1) * bounder_size + (j + 1) * height, k] = inputs[i * n_cols + j, :, :, k]
+    return image
+
+
+if __name__ == '__main__':
+    data = []
+    for i in range(16):
+        temp = np.random.random((28, 28, 3))
+        data.append(temp)
+    data = np.array(data)
+    print(data.shape)
+    m = build_image_(data, 4)
+    plt.imshow(m)
+    plt.show()
