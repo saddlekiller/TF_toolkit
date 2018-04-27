36f4ad73b142879636327b818ed287c272fabd02
cheng <alan_debug@icloud.com>
2018-04-27 15:08:19 +0800

    complete a simple comparison demo

--- models/msd_lstm/demo.py
+++ models/msd_lstm/demo.py
@@ -7,7 +7,7 @@ def basic_lstm(features, labels, mode):
 	n_lstm_hidden = 25
 	n_affine_hidden = 200
 	n_classes = 10
-	rnn_cell = tf.nn.rnn_cell.LSTMCell(n_lstm_hidden)
+	rnn_cell = tf.contrib.rnn.LSTMCell(n_lstm_hidden)
 	dynamic_rnn_outputs, dynamic_rnn_states = tf.nn.dynamic_rnn(cell = rnn_cell, inputs = features['x'], dtype = tf.float32)
 	affine1 = tf.layers.dense(dynamic_rnn_outputs[:,-1,:], units = n_affine_hidden, activation = tf.nn.relu)
 	affine2 = tf.layers.dense(affine1, units = n_affine_hidden, activation = tf.nn.relu)
@@ -44,23 +44,121 @@ def normalized_affine(features, labels, mode):
 	n_lstm_hidden = 25
 	n_affine_hidden = 200
 	n_classes = 10
-	# rnn_cell = tf.nn.rnn_cell.LSTMCell(n_lstm_hidden)
-	# dynamic_rnn_outputs, dynamic_rnn_states = tf.nn.dynamic_rnn(cell = rnn_cell, inputs = features['x'], dtype = tf.float32)
-	# affine1 = tf.layers.dense(dynamic_rnn_outputs[:,-1,:], units = n_affine_hidden, activation = tf.nn.relu)
-	# affine2 = tf.layers.dense(affine1, units = n_affine_hidden, activation = tf.nn.relu)
-	# affine3 = tf.layers.dense(affine2, units = n_affine_hidden, activation = tf.nn.relu)
-	# logits = tf.layers.dense(affine3, units = n_classes, activation = tf.identity)
+
+	reshape1 = tf.reshape(features['x'], [-1, 120 * 25])
+	with tf.variable_scope('affine1'):
+		affine_w1_ = tf.get_variable(name = 'w', shape = [3000, 1024], initializer = tf.truncated_normal_initializer)
+		affine_w1 = affine_w1_ / tf.reduce_sum(affine_w1_, 0)
+		affine_b1_ = tf.get_variable(name = 'b', shape = [1024], initializer = tf.truncated_normal_initializer)
+		affine_b1 = affine_b1_ / tf.reduce_sum(affine_b1_, 0)
+		affine1 = tf.nn.sigmoid(tf.add(tf.matmul(reshape1, affine_w1), affine_b1))
+
+	with tf.variable_scope('affine2'):
+		affine_w2_ = tf.get_variable(name = 'w', shape = [1024, 256], initializer = tf.truncated_normal_initializer)
+		affine_w2 = affine_w2_ / tf.reduce_sum(affine_w2_, 0)
+		affine_b2_ = tf.get_variable(name = 'b', shape = [256], initializer = tf.truncated_normal_initializer)
+		affine_b2 = affine_b2_ / tf.reduce_sum(affine_b2_, 0)
+		affine2 = tf.nn.sigmoid(tf.add(tf.matmul(affine1, affine_w2), affine_b2))
+
+	with tf.variable_scope('affine3'):
+		affine_w3_ = tf.get_variable(name = 'w', shape = [256, 10], initializer = tf.truncated_normal_initializer)
+		# affine_w3 = tf.nn.softmax(affine_w3_, 0)
+		affine_w3 = affine_w3_ / tf.reduce_sum(affine_w3_, 0)
+		affine_b3_ = tf.get_variable(name = 'b', shape = [10], initializer = tf.truncated_normal_initializer)
+		# affine_b3 = tf.nn.softmax(affine_b3_)
+		affine_b3 = affine_b3_ / tf.reduce_sum(affine_b3_, 0)
+
+
+		logits = tf.identity(tf.add(tf.matmul(affine2, affine_w3), affine_b3))
+
+	predictions = {
+		'classes': tf.argmax(input = logits, axis = 1),
+		'probs': tf.nn.softmax(logits, name='softmax_tensor')
+	}
+	eval_metric_ops = {
+		'accuracy': tf.metrics.accuracy(
+			labels = labels, predictions = predictions['classes'], name = 'accuracy'
+		)
+	}
+	if mode == tf.estimator.ModeKeys.PREDICT:
+		return tf.estimator.EstimatorSpec(mode = mode, predictions = predictions)
+
+	loss = tf.losses.sparse_softmax_cross_entropy(labels = labels, logits = logits)
+	# acc = tf.metrics.accuracy(
+	# 	labels = labels, predictions = predictions['classes'], name = 'accuracy'
+	# )
+
+	if mode == tf.estimator.ModeKeys.TRAIN:
+		optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
+		train_op = optimizer.minimize(loss = loss, global_step = tf.train.get_global_step())
+		return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops, predictions = predictions, train_op = train_op)
+
+	if mode == tf.estimator.ModeKeys.EVAL:
+		return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops, predictions = predictions)
+
+def partial_normalized_affine(features, labels, mode):
+	n_lstm_hidden = 25
+	n_affine_hidden = 200
+	n_classes = 10
 
 	reshape1 = tf.reshape(features['x'], [-1, 120 * 25])
 	with tf.variable_scope('affine1'):
 		affine_w1 = tf.get_variable(name = 'w', shape = [3000, 1024], initializer = tf.truncated_normal_initializer)
 		affine_b1 = tf.get_variable(name = 'b', shape = [1024], initializer = tf.truncated_normal_initializer)
-		affine1 = tf.nn.relu(tf.add(tf.matmul(reshape1, affine_w1), affine_b1))
+		affine1 = tf.nn.sigmoid(tf.add(tf.matmul(reshape1, affine_w1), affine_b1))
 
 	with tf.variable_scope('affine2'):
 		affine_w2 = tf.get_variable(name = 'w', shape = [1024, 256], initializer = tf.truncated_normal_initializer)
 		affine_b2 = tf.get_variable(name = 'b', shape = [256], initializer = tf.truncated_normal_initializer)
-		affine2 = tf.nn.relu(tf.add(tf.matmul(affine1, affine_w2), affine_b2))
+		affine2 = tf.nn.sigmoid(tf.add(tf.matmul(affine1, affine_w2), affine_b2))
+
+	with tf.variable_scope('affine3'):
+		affine_w3_ = tf.get_variable(name = 'w', shape = [256, 10], initializer = tf.truncated_normal_initializer)
+		affine_w3 = affine_w3_ / tf.reduce_sum(affine_w3_, 0)
+		affine_b3_ = tf.get_variable(name = 'b', shape = [10], initializer = tf.truncated_normal_initializer)
+		affine_b3 = affine_b3_ / tf.reduce_sum(affine_b3_, 0)
+		logits = tf.identity(tf.add(tf.matmul(affine2, affine_w3), affine_b3))
+
+	predictions = {
+		'classes': tf.argmax(input = logits, axis = 1),
+		'probs': tf.nn.softmax(logits, name='softmax_tensor')
+	}
+	eval_metric_ops = {
+		'accuracy': tf.metrics.accuracy(
+			labels = labels, predictions = predictions['classes'], name = 'accuracy'
+		)
+	}
+	if mode == tf.estimator.ModeKeys.PREDICT:
+		return tf.estimator.EstimatorSpec(mode = mode, predictions = predictions)
+
+	loss = tf.losses.sparse_softmax_cross_entropy(labels = labels, logits = logits)
+	# acc = tf.metrics.accuracy(
+	# 	labels = labels, predictions = predictions['classes'], name = 'accuracy'
+	# )
+
+	if mode == tf.estimator.ModeKeys.TRAIN:
+		optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
+		train_op = optimizer.minimize(loss = loss, global_step = tf.train.get_global_step())
+		return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops, predictions = predictions, train_op = train_op)
+
+	if mode == tf.estimator.ModeKeys.EVAL:
+		return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops, predictions = predictions)
+
+def pure_affine(features, labels, mode):
+	n_lstm_hidden = 25
+	n_affine_hidden = 200
+	n_classes = 10
+
+	reshape1 = tf.reshape(features['x'], [-1, 120 * 25])
+	with tf.variable_scope('affine1'):
+		affine_w1 = tf.get_variable(name = 'w', shape = [3000, 1024], initializer = tf.truncated_normal_initializer)
+		affine_b1 = tf.get_variable(name = 'b', shape = [1024], initializer = tf.truncated_normal_initializer)
+		affine1 = tf.nn.sigmoid(tf.add(tf.matmul(reshape1, affine_w1), affine_b1))
+
+	with tf.variable_scope('affine2'):
+		affine_w2 = tf.get_variable(name = 'w', shape = [1024, 256], initializer = tf.truncated_normal_initializer)
+		affine_b2 = tf.get_variable(name = 'b', shape = [256], initializer = tf.truncated_normal_initializer)
+		affine2 = tf.nn.sigmoid(tf.add(tf.matmul(affine1, affine_w2), affine_b2))
 
 	with tf.variable_scope('affine3'):
 		affine_w3 = tf.get_variable(name = 'w', shape = [256, 10], initializer = tf.truncated_normal_initializer)
@@ -92,8 +190,16 @@ def normalized_affine(features, labels, mode):
 	if mode == tf.estimator.ModeKeys.EVAL:
 		return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops, predictions = predictions)
 
+model_mapping = {
+	'basic_lstm': basic_lstm,
+	'pure_affine': pure_affine,
+	'normalized_affine': normalized_affine,
+	'partial_normalized_affine': partial_normalized_affine
+}
 
-def main(unused_argv):
+def main(argv):
+	# print(argv)
+
 	train_= np.load('../../data/MSD/msd-10-genre-train.npz')
 	valid_= np.load('../../data/MSD/msd-10-genre-valid.npz')
 
@@ -104,7 +210,9 @@ def main(unused_argv):
 
 
 	# msd_classifier = tf.estimator.Estimator(model_fn = basic_lstm, model_dir = './models/msd_model')
-	msd_classifier = tf.estimator.Estimator(model_fn = normalized_affine, model_dir = './models/msd_model')
+	# msd_classifier = tf.estimator.Estimator(model_fn = normalized_affine, model_dir = './models/msd_model_all_prob')
+
+	msd_classifier = tf.estimator.Estimator(model_fn = model_mapping[argv[1]], model_dir = './models/' + argv[2])
 
 	tensors_to_log = {"probabilities": "softmax_tensor"}
 	# logging_hook = tf.train.LoggingTensorHook(tensors = tensors_to_log, every_n_iter = 1)
@@ -126,7 +234,7 @@ def main(unused_argv):
 				shuffle = False)
 		return None
 
-	for i in range(1000):
+	for i in range(200):
 		msd_classifier.train(
 			input_fn = get_input_fn(train_inputs, train_targets, tf.estimator.ModeKeys.TRAIN),
 			steps = 5
@@ -138,3 +246,9 @@ def main(unused_argv):
 
 if __name__ == '__main__':
 	tf.app.run()
+	# temp = tf.constant(np.random.random((4, 2)))
+	# temp1 = tf.nn.softmax(temp, 0)
+	# # print(temp1)
+	# sess = tf.Session()
+	# print(sess.run(temp))
+	# print(sess.run(temp1))
---
+++ models/msd_lstm/run.sh
@@ -0,0 +1,4 @@
+python demo.py basic_lstm
+python demo.py pure_affine pure_affine
+python demo.py normalized_affine normalized_affine
+python demo.py partial_normalized_affine partial_normalized_affine
